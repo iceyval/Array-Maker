1341354ce9b893432f2019e80362ce72c5a7cb61
InPanda0 <118008313+InPanda0@users.noreply.github.com>
2023-03-10 20:33:52 +0000

    added stuff

---
+++ ArrayMaker/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("{");
+            foreach (string txt in File.ReadAllLines("TextFile1.txt"))
+            {
+                sb.Append("\"" + txt + "\"");
+                if(File.ReadAllLines("TextFile1.txt").Last() != txt)
+                {
+                    sb.Append(",");
+                }
+            }
+            sb.Append("};");
+            File.WriteAllText("Result.txt", sb.ToString());
+        }
+    }
+}
